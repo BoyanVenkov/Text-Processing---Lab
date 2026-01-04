4841bb4f3836fd4ef00663f9a2d499adfbda1230
Boyan Venkov <boyanvenkov@yahoo.com>
2026-01-04 10:44:54 +0200

    Added a New Project

---
+++ 04. Text Filter/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] bannedWords = Console.ReadLine()
+                                      .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+string text = Console.ReadLine();
+
+foreach (string word in bannedWords)
+{
+    string censored = new string('*', word.Length);
+    text = text.Replace(word, censored);
+}
+
+Console.WriteLine(text);
